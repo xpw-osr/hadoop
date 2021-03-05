54e6fba35763d26cc603e1992cad1c92246f226e
xpw-osr <x1npen9w@gmail.com>
2021-03-05 10:49:44 +0800

    move some files to parent module

--- start-services.sh
+++ start-services.sh
@@ -59,55 +59,50 @@ hdfs --daemon start secondarynamenode
 hdfs --daemon start datanode
 
 # change ownership and permission hdfs folders
+
+## secure mode
+# --------------------------------------------------
 # kinit -kt /etc/keytabs/master/nn.keytab nn/master
-# if (( $? == 0 )); then
-#   hdfs dfs -chown hdfs:hadoop /
-#   hdfs dfs -chmod 755 /
+# hdfs dfs -chown hdfs:hadoop /
+# hdfs dfs -chmod 755 /
+# hdfs dfs -mkdir /tmp
+# hdfs dfs -chown hdfs:hadoop /tmp
+# hdfs dfs -chmod 777 /tmp
+# hdfs dfs -mkdir /user
+# hdfs dfs -chown hdfs:hadoop /user
+# hdfs dfs -chmod 755 /user
+# hdfs dfs -mkdir -p /logs
+# hdfs dfs -chown yarn:hadoop /logs
+# hdfs dfs -chmod 777 /logs
+# hdfs dfs -mkdir -p /mr-history
+# hdfs dfs -chown hdfs:hadoop /mr-history
+# hdfs dfs -chmod 777 /mr-history
+# hdfs dfs -mkdir -p /mr-history/tmp
+# hdfs dfs -chown mapred:hadoop /mr-history/tmp
+# hdfs dfs -chmod 777 /mr-history/tmp
+# hdfs dfs -mkdir -p /mr-history/done
+# hdfs dfs -chown mapred:hadoop /mr-history/done
+# hdfs dfs -chmod 750 /mr-history/done
 
-#   hdfs dfs -mkdir /tmp
-#   hdfs dfs -chown hdfs:hadoop /tmp
-#   hdfs dfs -chmod 777 /tmp
-
-#   hdfs dfs -mkdir /user
-#   hdfs dfs -chown hdfs:hadoop /user
-#   hdfs dfs -chmod 755 /user
-
-#   hdfs dfs -mkdir -p /logs
-#   hdfs dfs -chown yarn:hadoop /logs
-#   hdfs dfs -chmod 777 /logs
-
-#   hdfs dfs -mkdir -p /mr-history
-#   hdfs dfs -chown hdfs:hadoop /mr-history
-#   hdfs dfs -chmod 777 /mr-history
-
-#   hdfs dfs -mkdir -p /mr-history/tmp
-#   hdfs dfs -chown mapred:hadoop /mr-history/tmp
-#   hdfs dfs -chmod 777 /mr-history/tmp
-
-#   hdfs dfs -mkdir -p /mr-history/done
-#   hdfs dfs -chown mapred:hadoop /mr-history/done
-#   hdfs dfs -chmod 750 /mr-history/done
-
-#   kdestroy
-# else  # non-secure mode
-  su -c 'hdfs dfs -chown hdfs:hadoop /' - hdfs
-  su -c 'hdfs dfs -chmod 755 /' - hdfs
-  su -c 'hdfs dfs -mkdir /tmp' - hdfs
-  su -c 'hdfs dfs -chown hdfs:hadoop /tmp' - hdfs
-  su -c 'hdfs dfs -chmod 777 /tmp' - hdfs
-  su -c 'hdfs dfs -mkdir /user' - hdfs
-  su -c 'hdfs dfs -chown hdfs:hadoop /user' - hdfs
-  su -c 'hdfs dfs -chmod 755 /user' - hdfs
-  su -c 'hdfs dfs -mkdir -p /logs' - hdfs
-  su -c 'hdfs dfs -chown yarn:hadoop /logs' - hdfs
-  su -c 'hdfs dfs -chmod 777 /logs' - hdfs
-  su -c 'hdfs dfs -mkdir -p /mr-history/tmp' - hdfs
-  su -c 'hdfs dfs -chown mapred:hadoop /mr-history/tmp' - hdfs
-  su -c 'hdfs dfs -chmod 777 /mr-history/tmp' - hdfs
-  su -c 'hdfs dfs -mkdir -p /mr-history/done' - hdfs
-  su -c 'hdfs dfs -chown mapred:hadoop /mr-history/done' - hdfs
-  su -c 'hdfs dfs -chmod 750 /mr-history/done' - hdfs
-# fi
+## non-secure mode
+# --------------------------------------------------
+su -c 'hdfs dfs -chown hdfs:hadoop /' - hdfs
+su -c 'hdfs dfs -chmod 755 /' - hdfs
+su -c 'hdfs dfs -mkdir /tmp' - hdfs
+su -c 'hdfs dfs -chown hdfs:hadoop /tmp' - hdfs
+su -c 'hdfs dfs -chmod 777 /tmp' - hdfs
+su -c 'hdfs dfs -mkdir /user' - hdfs
+su -c 'hdfs dfs -chown hdfs:hadoop /user' - hdfs
+su -c 'hdfs dfs -chmod 755 /user' - hdfs
+su -c 'hdfs dfs -mkdir -p /logs' - hdfs
+su -c 'hdfs dfs -chown yarn:hadoop /logs' - hdfs
+su -c 'hdfs dfs -chmod 777 /logs' - hdfs
+su -c 'hdfs dfs -mkdir -p /mr-history/tmp' - hdfs
+su -c 'hdfs dfs -chown mapred:hadoop /mr-history/tmp' - hdfs
+su -c 'hdfs dfs -chmod 777 /mr-history/tmp' - hdfs
+su -c 'hdfs dfs -mkdir -p /mr-history/done' - hdfs
+su -c 'hdfs dfs -chown mapred:hadoop /mr-history/done' - hdfs
+su -c 'hdfs dfs -chmod 750 /mr-history/done' - hdfs
 
 # start yarn
 yarn --daemon start resourcemanager
